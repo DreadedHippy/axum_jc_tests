b02f0df51557058a613fef2820f515c619d5c9f6
onotaizee@gmail.com <onotaizee@gmail.com>
2024-05-28 23:38:00 +0100

    feat: finished!

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -7,13 +7,21 @@ use serde_json::json;
 async fn quick_dev() -> Result<()> {
 	let hc = httpc_test::new_client("http://localhost:3000")?;
 
-	hc.do_get("/hello2/Mike").await?.print().await?;
+	// hc.do_get("/hello2/Mike").await?.print().await?;
 
 	hc.do_post("/api/login", json!({
 		"username": "demo1",
-		"pwd": "welcomeDDD"
+		"pwd": "welcome"
 	})).await?.print().await?;
 
+	let req_ceate_ticket = hc.do_post("/api/tickets", json!({
+		"title": "AAA"
+	}));
+
+	req_ceate_ticket.await?.print().await?;
+	hc.do_delete("/api/tickets/0").await?.print().await?;
+
+	hc.do_get("/api/tickets").await?.print().await?;
 	// hc.do_get("/src/main.rs").await?.print().await?;
 	Ok(())
 }
